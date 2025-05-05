095eadd30c1ac243e37cbc848691977573a3dbe3
Rafael <rpena06@calpoly.edu>
2025-05-05 14:43:54 -0700

    add files

---
+++ src/api/users.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends, status
+from pydantic import BaseModel, Field, field_validator
+from typing import List
+from src.api import auth
+import sqlalchemy
+from src import database as db
+
+router = APIRouter(prefix="/users",
+                  tags=["users"],
+                   dependencies=[Depends(auth.get_api_key)])
