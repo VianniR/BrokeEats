73dae2b939adb34cde14fc00e798d2a6e32824f3
Rafael <rpena06@calpoly.edu>
2025-05-08 18:32:36 -0700

    add preference reccs

--- src/api/users.py
+++ src/api/users.py
@@ -27,6 +27,21 @@ class ProfileUpdate(BaseModel):
     username: Optional[str] = Field(None, example="new_username")
     email: Optional[str] = Field(None, example="bob@example.com")
 
+class RestaurantRecommendation(BaseModel):
+    id: int
+    name: str
+    cuisine: str
+    address: str
+    city: str
+    state: str
+    zipcode: str
+    phone: Optional[str]
+    overall_score: Optional[float]
+    food_rating: Optional[float]
+    service_rating: Optional[float]
+    price_rating: Optional[float]
+    cleanliness_rating: Optional[float]
+
 @router.post("/profile", response_model=Profile)
 def create_profile(profile: NewUser) -> Profile:
     try:
@@ -49,17 +64,15 @@ def create_profile(profile: NewUser) -> Profile:
 
 
 
-@router.get("/profile/{id}", response_model=Profile)
-def get_profile(user_id: int):
-
-
+@router.get("/profile/{user_name}", response_model=Profile)
+def get_profile(user_name: int) -> Profile:
     try:
         with db.engine.begin() as connection:
             user = connection.execute(
                 sqlalchemy.text(
-                    "SELECT id, name, username, email, permissions FROM users WHERE users.id = :user_id"
+                    "SELECT id, name, username, email, permissions FROM users WHERE users.name = :user_name"
                 ),
-                {"user_id": user_id}
+                {"user_name": user_name}
             ).one()
     except sqlalchemy.exc.NoResultFound:
         raise HTTPException(status_code=404, detail="User not found")
@@ -93,3 +106,113 @@ def update_profile(user_id: int, payload: ProfileUpdate):
         raise HTTPException(status_code=409, detail="Username or email already exists")
 
     return user
+@router.get("/users/recommendations", response_model=List[RestaurantRecommendation])
+def get_preference_recc_username(user_name: str, limit: int) -> List[RestaurantRecommendation]:
+    restaurants: List[RestaurantRecommendation] = []
+
+    with db.engine.connect() as conn:
+        row = conn.execute(sqlalchemy.text("""
+        SELECT DISTINCT 
+        restaurants.id, 
+        restaurants.name, 
+        cuisines.name, 
+        address, 
+        city, 
+        state, 
+        zipcode, 
+        phone, 
+        sum(overall_rating), 
+        sum(food_rating), 
+        sum(service_rating), 
+        sum(price_rating), 
+        sum(cleanliness_rating)
+        FROM restaurants
+        JOIN cuisines ON cuisines.id = restaurants.cuisine_id
+        JOIN restaurant_preferences ON restaurant_preferences.restaurant_id = restaurants.id
+        JOIN preferences ON preferences.id = restaurant_preferences.preference_id
+        JOIN user_preferences ON user_preferences.preference_id = preferences.id
+        JOIN users on users.id = user_preferences.user_id
+        JOIN reviews ON reviews.restaurant_id = restaurants.id
+        WHERE users.username = :username
+        group by restaurants.id, restaurants.name, cuisines.name, address, city, state, zipcode, phone
+        ORDER BY sum(overall_rating) DESC
+        LIMIT :limit
+        """), [
+            {
+                "username": user_name,
+                "limit": limit,
+            }
+        ])
+        for r_id, name, cuisine, address, city, state, zipcode, phone, overall, food, service, price, cleanliness  in row:
+            restaurants.append(RestaurantRecommendation(
+                id=r_id,
+                name=name,
+                cuisine=cuisine,
+                address=address,
+                city=city,
+                state=state,
+                zipcode=zipcode,
+                phone=phone,
+                overall_score=overall,
+                food_rating=food,
+                service_rating=service,
+                price_rating=price,
+                cleanliness_rating=cleanliness))
+
+    return restaurants
+
+
+@router.get("/users/recommendations", response_model=List[RestaurantRecommendation])
+def get_preference_recc_id(user_id: int, limit: int) -> List[RestaurantRecommendation]:
+    restaurants: List[RestaurantRecommendation] = []
+
+    with db.engine.connect() as conn:
+        row = conn.execute(sqlalchemy.text("""
+        SELECT DISTINCT 
+        restaurants.id, 
+        restaurants.name, 
+        cuisines.name, 
+        address, 
+        city, 
+        state, 
+        zipcode, 
+        phone, 
+        sum(overall_rating), 
+        sum(food_rating), 
+        sum(service_rating), 
+        sum(price_rating), 
+        sum(cleanliness_rating)
+        FROM restaurants
+        JOIN cuisines ON cuisines.id = restaurants.cuisine_id
+        JOIN restaurant_preferences ON restaurant_preferences.restaurant_id = restaurants.id
+        JOIN preferences ON preferences.id = restaurant_preferences.preference_id
+        JOIN user_preferences ON user_preferences.preference_id = preferences.id
+        JOIN users on users.id = user_preferences.user_id
+        JOIN reviews ON reviews.restaurant_id = restaurants.id
+        WHERE users.id = :user_id
+        group by restaurants.id, restaurants.name, cuisines.name, address, city, state, zipcode, phone
+        ORDER BY sum(overall_rating) DESC
+        LIMIT :limit
+        """), [
+            {
+                "user_id": user_id,
+                "limit": limit,
+            }
+        ])
+        for r_id, name, cuisine, address, city, state, zipcode, phone, overall, food, service, price, cleanliness  in row:
+            restaurants.append(RestaurantRecommendation(
+                id=r_id,
+                name=name,
+                cuisine=cuisine,
+                address=address,
+                city=city,
+                state=state,
+                zipcode=zipcode,
+                phone=phone,
+                overall_score=overall,
+                food_rating=food,
+                service_rating=service,
+                price_rating=price,
+                cleanliness_rating=cleanliness))
+
+    return restaurants
