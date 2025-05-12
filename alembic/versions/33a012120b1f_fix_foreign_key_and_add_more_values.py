d5dd9ceb958716d008f649407b12893488338840
Rafael <rpena06@calpoly.edu>
2025-05-12 12:14:18 -0700

    fix foreign key for restaurant and drop cuisine_id from review

--- alembic/versions/33a012120b1f_fix_foreign_key_and_add_more_values.py
+++ alembic/versions/33a012120b1f_fix_foreign_key_and_add_more_values.py
@@ -29,15 +29,32 @@ def upgrade() -> None:
     op.execute("""INSERT INTO user_preferences (user_id, preference_id) VALUES (2, 2)""")
 
     op.execute(
-        """INSERT INTO restaurants (name, cuisine_id, address, city, state, zipcode, phone, last_updated_by) VALUES ('El Guero', 1, '1122 Chorro St', 'San Luis Obispo', 'CA', '93401', '805-540-4637', 1)""")
+        """INSERT INTO restaurants (name, cuisine_id, address, city, state, zipcode, phone) VALUES ('El Guero', 1, '1122 Chorro St', 'San Luis Obispo', 'CA', '93401', '805-540-4637')""")
     op.execute(
-        """INSERT INTO restaurants (name, cuisine_id, address, city, state, zipcode, phone, last_updated_by) VALUES ('Panda Express', 2, '789 Foothill Blvd', 'San Luis Obispo', 'CA', '93405', '805-784-0355', 1)""")
+        """INSERT INTO restaurants (name, cuisine_id, address, city, state, zipcode, phone) VALUES ('Panda Express', 2, '789 Foothill Blvd', 'San Luis Obispo', 'CA', '93405', '805-784-0355')""")
+
+    op.execute(
+        """INSERT INTO reviews (user_id, restaurant_id, overall_rating, food_rating, service_rating, price_rating, cleanliness_rating, written_review) VALUES (1, 1, 4.5, 5, 3, 5, 5, 'Nice')""")
+    op.execute(
+        """INSERT INTO reviews (user_id, restaurant_id, overall_rating, food_rating, service_rating, price_rating, cleanliness_rating, written_review) VALUES (1, 2, 3, 4, 5, 2, 1, 'Good')""")
+
+    op.execute(
+        """INSERT INTO restaurant_preferences (restaurant_id, preference_id, last_updated_by) VALUES (2, 1, 1)""")
+    op.execute(
+        """INSERT INTO restaurant_preferences (restaurant_id, preference_id, last_updated_by) VALUES (1, 1, 1)""")
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.drop_constraint('restaurant_cuisine_id_fkey1', 'restaurants', type_='foreignkey')
+    op.drop_constraint('restaurants_cuisine_id_fkey1', 'restaurants', type_='foreignkey')
     op.create_foreign_key(
         'restaurants_cuisine_id_fkey', 'restaurants', 'preferences', ['cuisine_id'], ['id'],)
     op.add_column('reviews',sa.Column("cuisine_id", sa.Integer, sa.ForeignKey("cuisines.id", ondelete="CASCADE"),nullable=False,),
  )
+    op.execute("""TRUNCATE user_preferences RESTART IDENTITY CASCADE""")
+    op.execute("""TRUNCATE restaurants RESTART IDENTITY CASCADE""")
+    op.execute("""TRUNCATE reviews RESTART IDENTITY CASCADE""")
+    op.execute("""TRUNCATE restaurant_preferences RESTART IDENTITY CASCADE""")
+
+
+
